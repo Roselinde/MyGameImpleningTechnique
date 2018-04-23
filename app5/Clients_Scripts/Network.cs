63ced3331b8cf3814beefa9ec49753fb23e70a3e
supanat <supanat.ji@gmail.com>
2018-04-23 15:00:19 +0700

    ClientScript SocketIO

---
+++ app5/Clients_Scripts/Network.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+using System;
+
+public class Network : MonoBehaviour {
+
+    static SocketIOComponent socket;
+
+    public GameObject playerPrefab;
+
+	// Use this for initialization
+	void Start () {
+
+        socket = GetComponent<SocketIOComponent>();
+        socket.On("open", OnConnected);
+        socket.On("spawn", OnSpawned);
+	}
+
+    private void OnSpawned(SocketIOEvent e)
+    {
+        print("spawned");
+        Instantiate(playerPrefab);
+    }
+
+    void OnConnected(SocketIOEvent e)
+    {
+        print("Connected");
+
+        JSONObject j = 
+            new JSONObject(JSONObject.Type.OBJECT);
+
+        j.AddField("id", "53XXXXX");
+        j.AddField("name", "Sumeth");
+
+        socket.Emit("checkin", j);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
